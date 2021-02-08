f6e56e8d0d5fc553549bd480a64d974e102afa35
BestTommygun <tac.peters@student.avans.nl>
2021-02-08 16:07:47 +0100

    added starting unity project
    small environment and 1 unit

---
+++ ActualRtsAttempt/Assets/Scripts/selection/Base/Selectable.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace SelectionSystem.Base
+{
+    /// <summary>
+    /// The Selectable class act as: 
+    /// <para> A base class from which custom classes can Inherit from. </para>
+    /// When it's inherited all the logic must be written down.
+    /// </summary>
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Collider), typeof(Rigidbody))]
+    public abstract class Selectable : MonoBehaviour, ISelectable
+    {
+        /// <inheritdoc/>
+        public abstract bool isSelected
+        {
+            get;
+        }
+
+        /// <inheritdoc/>
+        public abstract void Select();
+
+        /// <inheritdoc/>
+        public abstract void Deselect();
+    }
+}
---
+++ ActualRtsAttempt/Assets/Scripts/selection_component.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selection_component : MonoBehaviour
+{
+    void Start()
+    {
+       transform.GetChild(1).gameObject.SetActive(true);
+    }
+
+    private void OnDestroy()
+    {
+        transform.GetChild(1).gameObject.SetActive(false);
+    }
+}
